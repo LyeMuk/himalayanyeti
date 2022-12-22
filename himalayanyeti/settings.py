2c91861004f7cfd11c34356d55736adf5c48a4b2
LyeMuk <mukesh2337.k@gmail.com>
2022-12-23 02:22:13 +0530

    vercel idea dropped

--- himalayanyeti/settings.py
+++ himalayanyeti/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 # vercel config
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-lklarn_d1x!3gcwm1jys0f19mf-on38=yuqx0$1emw#nwu9-vs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '*']
 
 # Application definition
 
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'himalayanyeti.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # vercel config
-DATABASE_URL="postgresql://postgres:uVtAiO9CuNIosnjZ8ZqL@containers-us-west-144.railway.app:6620/railway"
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+# DATABASE_URL="postgresql://postgres:uVtAiO9CuNIosnjZ8ZqL@containers-us-west-144.railway.app:6620/railway"
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # Password validation
@@ -125,8 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
