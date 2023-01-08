c3b884ad64a5929a322d99b0942c93679dc3a25c
LyeMuk <mukesh2337.k@gmail.com>
2023-01-08 23:11:50 +0530

    attempt 1 to resolve static

--- himalayanyeti/settings.py
+++ himalayanyeti/settings.py
@@ -134,7 +134,7 @@ EMAIL_HOST_PASSWORD = "koixhqtpjrvqvrwc"
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
