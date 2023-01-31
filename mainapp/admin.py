438ccb02c70750975bdaefda9307bace9cb80377
LyeMuk <mukesh2337.k@gmail.com>
2023-01-31 12:35:21 +0530

    view date in admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,10 +3,13 @@ from .models import categories
 from .models import label
 from .models import article, subscriber
 
+# adding date in admin.py
+class articleAdmin(admin.ModelAdmin):
+    readonly_fields = ('bid','postdate', )
 
 # Register your models here.
 
-admin.site.register(article)
+admin.site.register(article,articleAdmin)
 admin.site.register(label)
 admin.site.register(categories)
 admin.site.register(subscriber)
